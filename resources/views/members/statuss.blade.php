8e87e33b5132d29e3f0274a35692304c64333493
tsao <tsaoyuan@gmail.com>
2023-10-13 15:14:12 +0800

    feat: get-status > **使用者不存在**時，渲染資料到瀏覽器上
    - 當前的 response 是一巢狀資料，也就是，response 是一包 data，我要拿出 data's key = 'data' 的資料，用 `data.data` 取得資料
      - 若 data's key = name，則用 `data.name` 取得資料
    - JS 取出物件的 key名稱 方法: `Object.keys()`
      - 當物件為 innerData 時，取得 key名稱 : `Object.keys(innerData)`
    - 已知 使用者不存在，則 if(innerData['status'] === 'NOT_EXIST') do...

--- resources/views/members/statuss.blade.php
+++ resources/views/members/statuss.blade.php
@@ -36,7 +36,7 @@
       .then(response => {
         // HTTP status code 在 300 及以上  
         if (!response.ok) {
-          // response status = 400 do.. 
+          // 參數錯誤 > response status = 400 do.. 
           if (response.status === 400) {
             // 用 response.json() 取得 status = 400 的 response
             // console.log(response.json());
@@ -66,10 +66,26 @@
         return response.json();
       })
       .then(data => {
-        // 處理 response 的數據 
-        console.log(data);
-
+        // 輸出資訊容器
         const outputElement = document.getElementById('data-list');
+        // 輸出資訊 data 內的 data 
+        const innerData = data.data;
+
+        // JS 取出物件的 key 方法: Object.keys()
+        const keys = Object.keys(innerData);
+        // JS 取出物件的 value 方法
+        // console.log(innerData[keys]);
+
+        // 放置 輸出資訊 data 容器
+        const msgItem = document.createElement('p');
+        // 處理 response 的數據 
+        // 使用者不存在
+        // status === NOT_EXIST 時: 
+        if (innerData['status'] === 'NOT_EXIST') {
+
+          msgItem.textContent = "使用者不存在";
+          outputElement.appendChild(msgItem);
+        }
 
         // JS 迴圈的另類用法 `for...in` 類似 PHP 的 foreach ($data as $key => $value) 
         // JS 的 const key 的變數 key 如同 PHP 的 $key
