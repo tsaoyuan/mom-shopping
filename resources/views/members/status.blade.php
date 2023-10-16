bfefd820e48ea42d0cb1e7d5bc6a7ce0a661cc69
tsao <tsaoyuan@gmail.com>
2023-10-16 11:16:43 +0800

    feat: 建立 狀態查詢畫面-輸入電話號碼

--- resources/views/members/status.blade.php
+++ resources/views/members/status.blade.php
@@ -3,9 +3,16 @@
 
 <head>
   <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+  <script src="{{ Vite::asset('resources/js/getStatus.js') }}"></script>
   <title>會員狀態</title>
 </head>
 <style>
+  .getStatus {
+    width: 600px;
+    margin-top: 1px;
+    margin-bottom: 10px;
+  }
+
   #title {
     width: 300px;
     background: #0f0;
@@ -20,11 +27,16 @@
 </style>
 
 <body>
+  <div class="getStatus">
+    <label for="mobile">手機號碼:</label>
+    <input name="mobile" id="mobile" type="text">
+    <button id="getStatus">查詢狀態</button>
+  </div>
+
   <div id="title">
     <span>Member status</span>
   </div>
   <ul id="data-list" class="data-list"></ul>
-  <script src="{{ Vite::asset('resources/js/getStatus.js') }}"></script>
 
   <script>
     const mobile = '0956632444';
