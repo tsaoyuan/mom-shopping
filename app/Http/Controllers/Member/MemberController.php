15130723e2960f5b7b4da6137207bf4fb0b538e8
tsao <tsaoyuan@gmail.com>
2023-10-30 14:33:42 +0800

    fix: RegisterController 改為 MemberController
    - 對於 Member 的 CRUD 皆在 `MemberController`

--- app/Http/Controllers/Member/MemberController.php
+++ app/Http/Controllers/Member/MemberController.php
@@ -7,13 +7,13 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Hash;
 
-class RegisterController extends Controller
+class MemberController extends Controller
 {
     public function register(Request $request){
         $validated = $this->validate($request, [
             'name' => ['required'], 
             'password' => ['required', 'alpha_num:ascii', 'min:6','max:12'],
-            'mobile' => ['required', 'string', 'max:14'], 
+            'mobile' => ['required', 'string', 'size:10', 'regex:/^[0-9]+$/'],
         ]);
 
         $member = Member::create(
