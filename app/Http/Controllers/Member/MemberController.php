193be4ea2601dcdef4b48321d7cee4f79b636236
tsao <tsaoyuan@gmail.com>
2023-10-30 14:58:38 +0800

    feat: 手動建立驗證器 (Validator)
    - 經由手動建立的 `Validator` 撈出 register 時，參數錯誤的訊息(`$validator->errors()`)

--- app/Http/Controllers/Member/MemberController.php
+++ app/Http/Controllers/Member/MemberController.php
@@ -6,15 +6,23 @@ use App\Models\Member;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
 
 class MemberController extends Controller
 {
     public function register(Request $request){
-        $validated = $this->validate($request, [
+        
+        // 手動建立驗證器 (Validator)
+        $validator = Validator::make($request->all(),[
             'name' => ['required'], 
             'password' => ['required', 'alpha_num:ascii', 'min:6','max:12'],
             'mobile' => ['required', 'string', 'size:10', 'regex:/^[0-9]+$/'],
         ]);
+
+        // 驗證器-驗證參數錯誤
+        if($validator->fails()){
+            dd($validator->errors());
+        }
 
         $member = Member::create(
             array_merge(
