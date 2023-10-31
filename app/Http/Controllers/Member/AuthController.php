bac677c122107d3cfb6d1a7cc62fd90b8df3ce84
tsao <tsaoyuan@gmail.com>
2023-10-31 17:28:48 +0800

    feat: Create member-login API

--- app/Http/Controllers/Member/AuthController.php
+++ app/Http/Controllers/Member/AuthController.php
@@ -4,34 +4,54 @@ namespace App\Http\Controllers\Member;
 
 use App\Models\Member;
 use Illuminate\Http\Request;
+use App\Helpers\SystemResponse;
 use App\Http\Controllers\Controller;
+use App\Http\Resources\Member\LoginResource;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
 
 class AuthController extends Controller
 {
     public function login(Request $request)
     {
+        // Body parameters
+        $validator = Validator::make(
+            $request->all(),
+            [
+                'mobile'   => ['required', 'string', 'size:10', 'regex:/^[0-9]+$/'],
+                'password' => ['required', 'string', 'min:4', 'max:8'],
+            ]
+        );
 
-        $validated = $this->validate($request, [
-            'mobile' => ['required'], 
-            // 'email' => ['required'],
-            // 'name' => ['required'],
-            'password' => ['required'],
-        ]);
-        // dd($validated);
+        // 參數格式錯誤回應
+        if ($validator->fails()) {
+            return SystemResponse::errorResponse($validator->errors());
+        }
 
+        // 用 mobile 查詢會員存在(顯示會員資料) or 不存在(null)
+        $member = Member::where('mobile', $request->input('mobile'))->first();
+
+        // 會員不存在
+        if (!$member) {
+            return SystemResponse::basicResponse('會員不存在', 404);
+        }
+
+        // 驗證成功，獲取已驗證的值 ($validator->validated())
+        $validated = $validator->validated();
         $token = Auth::attempt($validated);
-        $user = Auth::user();
-        // dd($user);
-        return response([
-            'token' => $token,
-            'data'  => $user
-        ]);
-        // return '會員登入';
+
+        // 帳號密碼錯誤 
+        if (!$token) {
+            return SystemResponse::basicResponse('帳號密碼錯誤', 400);
+        } else {
+            // 登入成功    
+            $member['token'] = $token;
+            return SystemResponse::dataResponse(LoginResource::make($member), '登入成功');
+        }
     }
 
-    public function logout(){
+    public function logout()
+    {
         // 清除使用者的認證狀態(JWT 工具產生的 token)
         Auth::logout();
         return response()->noContent();
