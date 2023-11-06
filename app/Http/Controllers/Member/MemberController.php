d0fde621f896774e545759f910b53f44e2a5f57d
tsao <tsaoyuan@gmail.com>
2023-11-06 15:30:27 +0800

    feat: 會員註冊時，新增 email 驗證
    - 媽媽購新增 寄信的功能，因此，註冊時增加 email 是否被註冊過的驗證/判斷

--- app/Http/Controllers/Member/MemberController.php
+++ app/Http/Controllers/Member/MemberController.php
@@ -9,6 +9,7 @@ use App\Http\Controllers\Controller;
 use App\Http\Resources\Member\RegisterResource;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
+use App\Http\Resources\Member\RegisterResource;
 
 /**
  * @group Member management / 會員管理
@@ -34,6 +35,9 @@ class MemberController extends Controller
             'password' => ['required', 'alpha_num:ascii', 'min:6','max:12'],
             // 電話 Example:0900000000
             'mobile' => ['required', 'string', 'size:10', 'regex:/^[0-9]+$/'],
+            // 信箱 Example:example@mail.com 
+            'email'    => ['required', 'email', 'max:255'
+            ],
         ]);
 
         // 驗證器-驗證參數錯誤
@@ -41,16 +45,18 @@ class MemberController extends Controller
             return SystemResponse::errorResponse($validator->errors()); 
         }
 
-        // 驗證成功，獲取已驗證的值 (name, password, mobile)
+        // 驗證成功，獲取已驗證的值 
         $validated = $validator->validated();
         $mobile = $validated['mobile'];
+        $email = $validated['email'];
 
-        // 判斷 mobile 是否已經被註冊過
-        $member = Member::where('mobile', $mobile)->first();
+        // 判斷 mobile 或 email 是否已經被註冊過
+        $member = Member::where('mobile', $mobile)
+        ->orWhere('email', $email)->first();
         
         if ($member) {
-            // 手機已經被註冊過
-            return SystemResponse::basicResponse('手機已存在', 409);
+            // 手機或信箱已經被註冊過
+            return SystemResponse::basicResponse('手機或信箱已存在', 409);
         }
 
         // 會員註冊成功
